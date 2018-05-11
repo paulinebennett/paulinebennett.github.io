7adaaad788ae97b8f534164948cf8d196e3bb2fa
Pauline Bennett <pauline.bennett@ericsson.com>
2018-05-11 15:00:37 -0400

    still fixing hand placement... not done yet

--- main.js
+++ main.js
@@ -1,9 +1,9 @@
 const centre = 200;
 
 function startTime() {
-	getCoordinates(centre - getElementById("second").getAttribute("y2"), "second");
-	getCoordinates(centre - getElementById("minute").getAttribute("y2"), "minute");
-	getCoordinates(centre - getElementById("hour").getAttribute("y2"), "hour");
+	getCoordinates(centre - document.getElementById("second").getAttribute("y2"), "second");
+	getCoordinates(centre - document.getElementById("minute").getAttribute("y2"), "minute");
+	getCoordinates(centre - document.getElementById("hour").getAttribute("y2"), "hour");
 }
 
 
@@ -18,13 +18,21 @@ function getCoordinates(radius, hand) {
 	
 	var date = new Date();
 	
+	console.log("Date " + date.toString());
+	console.log("Hours " + date.getHours().toString());
+	console.log("Minutes " + date.getMinutes().toString());
+	console.log("seconds " + date.getSeconds().toString());
+	
+	
 	if (hand == "second") {
 		var degsPastNoon = date.getSeconds() * degSecHand;
 	} else if (hand == "minute") {
 		var degsPastNoon =  date.getMinutes() * secPerMin * degMinHand + 
 							date.getSeconds() * degMinHand;
 	} else { //hand == "hour"
-		var degsPastNoon =  date.getHours() * secPerHour * degHourHand +
+		var hours = date.getHours();
+		if (hours > 12) { hours -= 12 }; 
+		var degsPastNoon =  hours * secPerHour * degHourHand +
 		                    date.getMinutes() * secPerMin * degHourHand + 
 							date.getSeconds() * degHourHand;
 	}
@@ -32,25 +40,60 @@ function getCoordinates(radius, hand) {
 	if (degsPastNoon < 90) {
 		var deg = 90 - degsPastNoon;
 	} else { //degsPastNoon >= 90
-	    var deg = 360 - (degsPastNoon - 90);
+	    var deg = 360 - degsPastNoon + 90;
 	}
 	
-	/*          .(x,y)
-     *		|  /
-	 *      | / <
-	 * _____|/deg\__
-	 *      |
-	 *      |
-	 *      |
+	console.log(hand + " degrees past noon: " + degsPastNoon.toString() + ", actual: " + deg.toString());
+	
+	/* Quadrant number, which trig functions are positive  
+	 *                 90
+	 *     II  (S)     |        I  (A)
+	 *                 |
+	 *                 |
+     *         	       |    .(x,y)
+     *		           |  /_
+	 *                 | /  \
+	 * 180_____________|/deg_\_____________0 (360)
+	 *                 |
+	 *                 |
+	 *                 |
+	 *     III (T)     |       IV  (C)
+	 *                 |
+	 *                270
 	 *
+	 * -need to find reference angle
+	 * -need to know which quadrant has the "triangle"
+	 *
+	 * 
 	 * r * cos(deg) = x from centre
 	 * r * sin(deg) = y from centre
 	 * 
 	 * (centre is 200,200)
+	 *
+	 *
 	 */
 	
-	var x = radius * Math.cos(deg) + centre;
-	var y = radius * Math.sin(deg) + centre;
+	//convert to radians for cos and sin functions
+	var rad = deg * (Math.PI / 180);
+	console.log("radians: " + rad.toString())
+	
+	if (deg <= 90) { //first
+		var cos = Math.cos(rad);
+		var sin = Math.sin(rad);
+	} else if (deg <= 180) { //second
+		var cos = -Math.cos(rad);
+		var sin = Math.sin(rad);
+	} else if (deg <= 270) { //third
+		var cos = -Math.cos(rad);
+		var sin = -Math.sin(rad);
+	} else { //fourth
+		var cos = Math.cos(rad);
+		var sin = -Math.sin(rad);
+	}
+	var x = radius * cos + centre;
+	var y = radius * sin + centre;			//coordinates need work (+ or - centre?)
+	
+	console.log("coords: x: " + x.toString() + " y:" + y.toString());
 	
 	document.getElementById(hand).setAttribute("x2", x.toString());
 	document.getElementById(hand).setAttribute("y2", y.toString());
